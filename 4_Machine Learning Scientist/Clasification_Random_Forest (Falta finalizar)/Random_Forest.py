1bfa7fd7419e164e8848440db23ec82d51fb5578
Charly1046 <corderodedios182@gmail.com>
2020-05-23 01:23:20 -0500

    Terminar

--- 4_Machine Learning Scientist/Clasification_Random_Forest (Falta finalizar)/Random_Forest.py
+++ 4_Machine Learning Scientist/Clasification_Random_Forest (Falta finalizar)/Random_Forest.py
@@ -5,14 +5,21 @@ Created on Thu May  7 20:58:30 2020
 
 @author: carlos
 """
+import numpy as np
 import pandas as pd
 import seaborn as sns
-import plotly.express as px
 
 from sklearn.tree import DecisionTreeClassifier # Import DecisionTreeClassifier
+from sklearn.linear_model import  LogisticRegression 
 from sklearn.model_selection import train_test_split # Import train_test_split
 from sklearn.metrics import accuracy_score # Import accuracy_score
 from sklearn.preprocessing import LabelEncoder
+
+import matplotlib.pyplot as plt
+from mlxtend.plotting import plot_decision_regions
+import matplotlib.gridspec as gridspec
+import itertools
+from dtreeviz.trees import *
 
 #Import dataset
 base = pd.read_csv("https://assets.datacamp.com/production/repositories/1796/datasets/0eb6987cb9633e4d6aa6cfd11e00993d2387caa4/wbc.csv")
@@ -21,8 +28,6 @@ base.shape
 list(enumerate(base.keys()))
 
 #Vizualitation
-tmp = ['purple' if (radius_mean > 15) & (point_mean > .025) else 'navy' for radius_mean, point_mean in zip(base.radius_mean, base['concave points_mean']) ] #Colores
-
 fig = sns.scatterplot(x = base['radius_mean'],
                   y = base['concave points_mean'],
                   hue = base['diagnosis'],
@@ -65,9 +70,17 @@ print(y_pred_tree)
 acc = accuracy_score(y_test, y_pred_tree)
 print("Test set accuracy: {:.2f}".format(acc))
 
-    #LogisticRegression
+# 6 - Arbol de desicion 
+viz = dtreeviz(dt,
+               X_train,
+               y_train,
+               target_name = 'diagnosis',
+               feature_names = ['concave points_mean','radius_mean'],
+               class_names = ['B','M'])
+              
+viz.view()       
 
-from sklearn.linear_model import  LogisticRegression
+    #LogisticRegression
 
 # 2 - Train your first classification LogisticRegression
 logreg = LogisticRegression(random_state=1)
@@ -83,28 +96,42 @@ acc = accuracy_score(y_test, y_pred_log)
 print("Test set accuracy: {:.2f}".format(acc))
 
 # Define a list called clfs containing the two classifiers logreg and dt
-clfs = [logreg, dt]
+clfs = [dt, logreg]
 
-#
+#Function graph regions models
+def plot_labeled_decision_regions(X_test , y_test, clfs):
+
+    gs = gridspec.GridSpec(2, 2)
+    
+    fig = plt.figure(figsize=(10,8))
+    
+    labels = ['Desicio Tree Classfier', 'Logistic Regression']
+    
+    for clfs, lab, grd in zip([dt, logreg], labels, itertools.product([0, 1], repeat=2)):
+    
+        clfs.fit(np.array(X_test), np.array(y_test))
+        
+        ax = plt.subplot(gs[grd[0], grd[1]])
+        
+        fig = plot_decision_regions(X = np.array(X_test),
+                                    y = np.array(y_test),
+                                    clf=clfs,
+                                    legend=2,
+                                    zoom_factor = 20)
+        
+        plt.title(lab)
+    
+    plt.show()
+
 # Review the decision regions of the two classifiers
-def plot_labeled_decision_regions(model_1,model_2):
-    y_pred_tree
-    y_pred_log
-    return ""
+plot_labeled_decision_regions(X_train, y_train, clfs)
 
-plot_labeled_decision_regions(X_test, y_test, clfs)
+#Criterios de entropia y gini
 
+# Instantiate dt_entropy, set 'entropy' as the information criterion
+dt_entropy = DecisionTreeClassifier(max_depth=8, criterion='entropy', random_state=1)
 
-
-
-
-
-
-
-
-
-
-
-
+# Fit dt_entropy to the training set
+dt_entropy.fit(X_train, y_train)
 
 
