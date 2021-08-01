91eabc87daf16ed7d4673fd3a64d1fb48cceceb2
Corderodedios182 <corderodedios182@gmail.com>
2021-08-01 13:52:45 -0500

    Algoritmo : selection-sort O(n x n)

--- 14_Algorithms/selection-sort.py
+++ 14_Algorithms/selection-sort.py
@@ -1,10 +1,36 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jul 30 14:45:38 2021
 
 @author: crf005r
+
 """
 
-#
-def selection_sort():
-    print("a")
+arr = [120,134,24,43,54,213,1]
+
+def findSmallest(arr):
+    """Look for the smallest element"""
+    smallest = arr[0]
+    smallest_index = 0
+    
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+            
+    return smallest_index
+
+def selectionSort(arr):
+    """Sorting algorithm by selection
+        Big O notation equal O(n x n)
+    """
+    newArr = []
+    
+    for i in range(len(arr)):
+        smallest = findSmallest(arr) #Stores the smallest value
+        newArr.append(arr.pop(smallest)) #Stores the index of the smallest value
+    
+    return newArr
+
+findSmallest(arr)
+
+selectionSort(arr)
