1598d5f9e7f76b3227d3a3f7a0f1cf2340304472
Corderodedios182 <corderodedios182@gmail.com>
2021-09-05 00:15:01 -0500

    ML : Metodo de Ensamble

--- 4_Machine Learning Scientist/Clasification_Random_Forest (Falta finalizar)/Random_Forest.py
+++ 4_Machine Learning Scientist/Clasification_Random_Forest (Falta finalizar)/Random_Forest.py
@@ -204,6 +204,61 @@ RMSE_train = (MSE(y_train, y_pred_train))**(1/2)
 # Print RMSE_train
 print('Train RMSE: {:.2f}'.format(RMSE_train))
 
+##Metodo de Ensamble
+# Import functions to compute accuracy and split data
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+# Import models, including VotingClassifier meta-model
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier as KNN
+from sklearn.ensemble import VotingClassifier
+
+# Set seed for reproducibility
+SEED=1
+
+# Instantiate lr
+lr = LogisticRegression(random_state=SEED)
+
+# Instantiate knn
+knn = KNN(n_neighbors=27)
+
+# Instantiate dt
+dt = DecisionTreeClassifier(min_samples_leaf=0.13, random_state=SEED)
+
+# Define the list classifiers
+classifiers = [('Logistic Regression', lr), ('K Nearest Neighbours', knn), ('Classification Tree', dt)]
+
+# Iterate over the pre-defined list of classifiers
+for clf_name, clf in classifiers:    
+  
+    # Fit clf to the training set
+    clf.fit(X_train, y_train)    
+  
+    # Predict y_pred
+    y_pred = clf.predict(X_test)
+    
+    # Calculate accuracy
+    accuracy = accuracy_score(y_test, y_pred)
+  
+    # Evaluate clf's accuracy on the test set
+    print('{:s} : {:.3f}'.format(clf_name, accuracy))
 
 
+# Import VotingClassifier from sklearn.ensemble
+from sklearn.ensemble import VotingClassifier
+
+# Instantiate a VotingClassifier vc 
+vc = VotingClassifier(estimators=classifiers)     
+
+# Fit vc to the training set
+vc.fit(X_train, y_train)   
+
+# Evaluate the test set predictions
+y_pred = vc.predict(X_test)
+
+# Calculate accuracy score
+accuracy = accuracy_score(y_test, y_pred)
+print('Voting Classifier: {:.3f}'.format(accuracy))
 
