ad5190a54b41fe4061e3a51626501eddb2ed61a8
Corderodedios182 <corderodedios182@gmail.com>
2021-09-19 00:04:53 -0500

    ML : Out of Bag Evaluation

--- 4_Machine Learning Scientist/Clasification_Random_Forest (Falta finalizar)/Random_Forest.py
+++ 4_Machine Learning Scientist/Clasification_Random_Forest (Falta finalizar)/Random_Forest.py
@@ -287,8 +287,37 @@ y_pred = bc.predict(X_test)
 acc_test = accuracy_score(y_test, y_pred)
 print('Test set accuracy of bc: {:.2f}'.format(acc_test)) 
 
+#- Out of Bag Evaluation
 
+# Import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier
 
+# Import BaggingClassifier
+from sklearn.ensemble import BaggingClassifier
+
+# Instantiate dt
+dt = DecisionTreeClassifier(min_samples_leaf=8, random_state=1)
+
+# Instantiate bc
+bc = BaggingClassifier(base_estimator=dt, 
+            n_estimators=50,
+            oob_score=True,
+            random_state=1)
+
+# Fit bc to the training set 
+bc.fit(X_train, y_train)
+
+# Predict test set labels
+y_pred = bc.predict(X_test)
+
+# Evaluate test set accuracy
+acc_test = accuracy_score(y_pred, y_test)
+
+# Evaluate OOB accuracy
+acc_oob = bc.oob_score_
+
+# Print acc_test and acc_oob
+print('Test set accuracy: {:.3f}, OOB accuracy: {:.3f}'.format(acc_test, acc_oob))
 
 
 
