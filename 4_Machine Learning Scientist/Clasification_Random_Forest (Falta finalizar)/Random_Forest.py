cbc795fd4a1de1fbff6a5f243fa24c12a23e69ff
Corderodedios182 <corderodedios182@gmail.com>
2021-09-19 21:11:04 -0500

    ML : Stochastic Gradient Boosting

--- 4_Machine Learning Scientist/Clasification_Random_Forest (Falta finalizar)/Random_Forest.py
+++ 4_Machine Learning Scientist/Clasification_Random_Forest (Falta finalizar)/Random_Forest.py
@@ -403,15 +403,62 @@ print('ROC AUC score: {:.2f}'.format(ada_roc_auc))
 
 ####Gradient Boosting
 
+# Import GradientBoostingRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+
+# Instantiate gb
+gb = GradientBoostingRegressor(n_estimators=200, 
+            max_depth=4,
+            random_state=2)
+
+# Fit gb to the training set
+gb.fit(X_train, y_train)
+
+# Predict test set labels
+y_pred = gb.predict(X_test)
+
+# Import mean_squared_error as MSE
+from sklearn.metrics import mean_squared_error as MSE
+
+# Compute MSE
+mse_test = MSE(y_test,y_pred)
+
+# Compute RMSE
+rmse_test = mse_test**(1/2)
+
+# Print RMSE
+print('Test set RMSE of gb: {:.3f}'.format(rmse_test))
 
 
+#####Stochastic Gradient Boosting (SGB)
 
+# Import GradientBoostingRegressor
+from sklearn.ensemble import GradientBoostingRegressor
 
+# Instantiate sgbr
+sgbr = GradientBoostingRegressor(max_depth=4, 
+            subsample=.9,
+            max_features=.75,
+            n_estimators=200,                                
+            random_state=2)
 
+# Fit sgbr to the training set
+sgbr.fit(X_train,y_train)
 
+# Predict test set labels
+y_pred = sgbr.predict(X_test)
 
+# Import mean_squared_error as MSE
+from sklearn.metrics import mean_squared_error as MSE
 
+# Compute test set MSE
+mse_test = MSE(y_test, y_pred)
 
+# Compute test set RMSE
+rmse_test = mse_test**(1/2)
+
+# Print rmse_test
+print('Test set RMSE of sgbr: {:.3f}'.format(rmse_test))
 
 
 
