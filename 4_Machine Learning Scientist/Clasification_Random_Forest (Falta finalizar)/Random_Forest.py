e9687944e80d45cbd079b6b19c611a1a83709ed1
Corderodedios182 <corderodedios182@gmail.com>
2021-09-20 23:31:26 -0500

    ML : Evaluate, Search hyperparameters tree

--- 4_Machine Learning Scientist/Clasification_Random_Forest (Falta finalizar)/Random_Forest.py
+++ 4_Machine Learning Scientist/Clasification_Random_Forest (Falta finalizar)/Random_Forest.py
@@ -473,12 +473,46 @@ dt = DecisionTreeClassifier(random_state=SEED)
 
 dt.get_params()
 
+##Aproaches to hyperparameter tuning 
 
+#Grid Search
 
+# Import GridSearchCV
+from sklearn.model_selection import GridSearchCV
 
+# Define the grid of hyperparameters 'params_dt'
+params_dt = {'max_depth': [3, 4,5, 6],
+             'min_samples_leaf': [0.04, 0.06, 0.08],
+             'max_features': [0.2, 0.4,0.6, 0.8]}
 
+# Instantiate a 10-fold CV grid search object 'grid_dt'
+grid_dt = GridSearchCV(estimator=dt,
+                       param_grid=params_dt,
+                       scoring='accuracy',
+                       cv=10,
+                       n_jobs=-1)
 
+# Fit 'grid_dt' to the training data
+grid_dt.fit(X_train, y_train)
 
+# Extract best hyperparameters from 'grid_dt'
+best_hyperparams = grid_dt.best_params_
+
+print('Best hyerparameters:\n', best_hyperparams)
+
+# Extract best CV score from 'grid_dt'
+best_CV_score = grid_dt.best_score_
+
+'Best CV accuracy {}'.format(best_CV_score)
+
+# Extract best model from 'grid_dt'
+best_model = grid_dt.best_estimator_
+
+# Evaluate test set accuracy
+test_acc = best_model.score(X_test,y_test)
+
+# Print test set accuracy
+print("Test set accuracy of best model: {:.3f}".format(test_acc))
 
 
 
