c2113b63831c5eca1d43734c2d75b6bfabbad9e5
Corderodedios182 <corderodedios182@gmail.com>
2021-09-23 08:38:58 -0500

    ML : Hyperparameters Grid

--- 4_Machine Learning Scientist/Clasification_Random_Forest (Falta finalizar)/Random_Forest.py
+++ 4_Machine Learning Scientist/Clasification_Random_Forest (Falta finalizar)/Random_Forest.py
@@ -514,40 +514,37 @@ test_acc = best_model.score(X_test,y_test)
 # Print test set accuracy
 print("Test set accuracy of best model: {:.3f}".format(test_acc))
 
+## - Hyperparameters Grid
 
+# Define the dictionary 'params_rf'
+params_rf = {'n_estimators': [100, 350, 500],
+             'max_features': ['log2','auto','sqrt'],
+             'min_samples_leaf': [2,10,30]}
 
+# Import GridSearchCV
+from sklearn.model_selection import  GridSearchCV
 
+# Instantiate grid_rf
+grid_rf = GridSearchCV(estimator=rf,
+                       param_grid=params_rf,
+                       scoring='neg_mean_squared_error',
+                       cv=3,
+                       verbose=1,
+                       n_jobs=-1)
 
+# Import mean_squared_error from sklearn.metrics as MSE 
+from sklearn.metrics import mean_squared_error as MSE
 
+grid_rf.fit(X_train, y_train)
 
+# Extract the best estimator
+best_model = grid_rf.best_estimator_
 
+# Predict test set labels
+y_pred = best_model.predict(X_test)
 
+# Compute rmse_test
+rmse_test = MSE(y_test, y_pred)**(1/2)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Print rmse_test
+print('Test RMSE of best model: {:.3f}'.format(rmse_test)) 
