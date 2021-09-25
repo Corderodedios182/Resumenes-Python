7f7cf910e07bc02fd6801110adca193228d5627b
Corderodedios182 <corderodedios182@gmail.com>
2021-09-25 11:49:15 -0500

    ML : Comparando modelos

--- 4_Machine Learning Scientist/Linear Classifier in Python.py
+++ 4_Machine Learning Scientist/Linear Classifier in Python.py
@@ -12,35 +12,45 @@ Created on Sat Sep 25 10:29:03 2021
 import sklearn.datasets
 
 newsgroup = sklearn.datasets.fetch_20newsgroups_vectorized()
-
 X, y = newsgroup.data, newsgroup.target 
-
 X.shape
-
 y.shape
+
+import pandas as pd
+X_dataframe = pd.DataFrame(X.todense())
 
 #Prediccion 
 from sklearn.neighbors import KNeighborsClassifier
 
-knn = KNeighborsClassifier(n_neighbors=1)
+#Instanciando 2 modelos diferentes
+knn_1 = KNeighborsClassifier(n_neighbors=1)
+knn_2 = KNeighborsClassifier(n_neighbors=2)
 
-knn.fit(X,y)
+#Entrenando el modelo con todos los datos
+knn_1.fit(X,y)
 
-y_pred = knn.predict(X)
+y_pred = knn_1.predict(X)
 
 #Evaluacion del modelo, erronea ya que debemos ver la presicion con datos invisibles.
-knn.score(X,y)
+knn_1.score(X,y)
 
 #Separando datos de entrenamiento y test
 from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X,y)
 
-knn.fit(X_train,y_train)
+#Entrenando 2 modelos
+knn_1.fit(X_train,y_train)
+knn_2.fit(X_train,y_train)
 
-knn.score(X_test, y_test)
+# Predict on the test features, print the results
+pred_1 = knn_1.predict(X_test)
+pred_2 = knn_2.predict(X_test)
 
+knn_1.score(X_test, y_test)
+knn_2.score(X_test, y_test)
 
-
-
-
+from sklearn.metrics import accuracy_score 
+# Este accuracy es lo mismo que knn.score
+accuracy_score(pred_1, y_test)
+accuracy_score(pred_2, y_test)
