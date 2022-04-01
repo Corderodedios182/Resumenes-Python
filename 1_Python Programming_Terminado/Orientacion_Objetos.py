30d77188974fb1a419b2dd8d09e010acecacc046
Corderodedios182 <corderodedios182@gmail.com>
2022-04-01 13:24:46 -0600

    POO : Introduction POO

---
+++ 1_Python Programming_Terminado/Orientacion_Objetos.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  1 12:16:21 2022
+
+@author: Maste
+"""
+
+#Clase
+
+class Auto:
+    
+    marca = ""
+    modelo = 2004
+    placa = ""
+    
+taxi = Auto()
+
+taxi.modelo
+
+class nombre:
+    pass
+
+#Pertenecen a la clase nombre
+carlos = nombre()
+maria = nombre()
+
+#sus atributos son : objeto.atributo = valor
+
+carlos.edad = 30
+carlos.sexo = 'masculino'
+carlos.pais = 'bolivia'
+
+maria.edad = 29
+maria.sexo = 'femenino'
+maria.pais = 'mexico'
+
+
+
+
+
+
+
+
+
+
+
+
