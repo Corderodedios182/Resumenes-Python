d8b7bed647548a9b9439e0a74d92972948aad4c5
Corderodedios182 <corderodedios182@gmail.com>
2022-04-03 15:37:56 -0500

    POO : Herencia Calulator

--- 15_Objet_Oriented_Programming/POO.py
+++ 15_Objet_Oriented_Programming/POO.py
@@ -145,20 +145,43 @@ nuevo_pokemon = pikachu('boby', 'electrico')
 nuevo_pokemon.__dict__
 nuevo_pokemon.descripcion()
     
+#Herencia ejemplo practico
 
+class Calculadora:
+    def __init__(self, numero):
+        self.n = numero
+        self.datos = [0 for i in range(numero)]
+        
+    def ingresardato(self):
+        self.datos = [int(input('ingresar datos ' + str(i+1) + ' = ')) for i in range(self.n)]
 
+class op_basicas(Calculadora):
+    def __init__(self):
+        Calculadora.__init__(self,2)
+        
+    def suma(self):
+        a,b = self.datos
+        s = a + b
+        print('El resultado es : ', s)
+        
+    def resta(self):
+        a,b = self.datos
+        s = a - b
+        print('El resultado es : ', s)
 
-    
+class raiz(Calculadora):
+    def __init__(self):
+        Calculadora.__init__(self, 1)
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
+    def cuadrada(self):
+        import math
+        a, = self.datos
+        print('El resultado es : ', math.sqrt(a))
 
+ejemplo = op_basicas()
+print(ejemplo.ingresardato())
+print(ejemplo.suma())
 
-
+ejemplo = raiz()
+print(ejemplo.ingresardato())
+print(ejemplo.cuadrada())
