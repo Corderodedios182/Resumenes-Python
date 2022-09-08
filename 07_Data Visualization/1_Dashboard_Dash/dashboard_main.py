8a3e1f33ff1dde55e390ef35c44b4812bff30057
Carlos Roberto Flores Luna <cflorelu@emeal.nttdata.com>
2022-09-07 20:47:31 -0500

    Dash : Interfaz Dash Plotly

--- 07_Data Visualization/1_Dashboard_Dash/dashboard_main.py
+++ 07_Data Visualization/1_Dashboard_Dash/dashboard_main.py
@@ -1,48 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  7 20:24:43 2022
+
+@author: cflorelu
+"""
+
+import plotly.graph_objects as go # or plotly.express as px
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import plotly.express as px
-from dash import Output, Input
+from dash import Output, Input, dash_table
 
-import dask.dataframe as dd
+import plotly.graph_objects as go
 
-#Carga de datos
-df = dd.read_csv('data/df_signal.csv').compute()
+#Bases Output (ARG_Dask_signals_process.py)
 
-# Create the Dash app
-app = dash.Dash(__name__)
+df_day = pd.read_csv("data/df_day.csv")
+df_day["signal"] = df_day["llave_comparativa"].str.split("|",expand=True).iloc[:,0]
 
-# Set up the layout using an overall div
-app.layout = html.Div(
-    children=[
-        dcc.Dropdown(id = 'title_dd',
-                     options = [{'label':'signal 1',
-                                'value':'hsa12_loopout_dsrsprtrdactpst_C1075052648'},
-                                {'label':'signal 2',
-                                 'value':'hsa12_loopout_eslsprtrdactpst_C1075052642'}
-                                ]),
-        dcc.Graph(id='my_graph')
-        ])
+df_missing_groups = pd.read_csv("data/df_missing_groups.csv")
+df_outlier = pd.read_csv("data/df_outlier.csv")
+
+signals = list(set(df_day["signal"]))
+
+app = dash.Dash()
+app.layout = html.Div([
+    dcc.Dropdown(signals, id ='input_select'),
+    dcc.Graph(id = 'fig')
+])
 
 @app.callback(
-    Output(component_id = 'my_graph',
+    Output(component_id = 'fig',
            component_property = 'figure'),
-    Input(component_id = 'title_dd',
+    Input(component_id = 'input_select',
            component_property = 'value')
      )
 def update_layouts(selection):
 
     title = 'None'
     if selection:
+        
         title = selection
-     
-    bar_graph = px.histogram(data_frame = df,
-                       x = title,
-                       title=f'{title}')
+        
+    fig = go.Figure()
 
-    return bar_graph
+    df_box = df_day[df_day["llave_comparativa"].str.contains(title)]
 
-if __name__ == '__main__':
-    app.run_server()
+    fig.add_trace(go.Box(
+        x = df_box["llave_comparativa"],
+        y = df_box["value"],
+        name='kale',
+        boxpoints='all',
+        jitter=0.5,
+        whiskerwidth=0.2,
+        marker_size=2,
+        line_width=1)
+        )
     
+    return fig
+
+app.run_server(debug=True, use_reloader=False)  # Turn off reloader if inside Jupyter
