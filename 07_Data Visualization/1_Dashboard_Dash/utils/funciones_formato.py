83407b4f6c314e4ba2be395a7d010ae6274f7f68
Carlos Roberto Flores Luna <cflorelu@emeal.nttdata.com>
2022-09-07 20:46:20 -0500

    Dash : Funciones formato de datos

--- 07_Data Visualization/1_Dashboard_Dash/utils/funciones_formato.py
+++ 07_Data Visualization/1_Dashboard_Dash/utils/funciones_formato.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Sep  2 11:56:10 2022
-
-@author: cflorelu
+Funciones de procesamiento de datos para las señales
 """
 
 import dask
 import pandas as pd
 import numpy as np
+import dask.dataframe as dd
 
 @dask.delayed
 def format_(df):
     """Formato a las señales extraidas.
        Agrupa por segundo para disminuir los datos de milisegundos a segundos.
+       Crea etiquetas y grupos de ancho, velocidad y grado de acero.
     """
     df['Time'] = df["Time"].dt.year.astype(str)  +"-" + \
                  df["Time"].dt.month.astype(str) +"-" + \
@@ -44,15 +44,60 @@ def format_(df):
 
     df["ancho_slab"] = df[ancho_slab]
     df["ancho_slab"] = np.select(filters, values)
-    df["ancho_slab"] = df["ancho_slab"].astype(str)
 
-    df["velocidad_linea"] = df[velocidad_linea].apply(lambda x: round(x, 1))
-    df["velocidad_linea"] = df["velocidad_linea"].astype(str)
+    df["velocidad_linea"] = df[velocidad_linea]#.apply(lambda x: round(x, 1))
 
     df["grado_acero"] = df[grado_acero]
-    df["grado_acero"] = df["grado_acero"].astype(str)
     
     df = df.drop([ancho_slab, velocidad_linea, grado_acero], axis = 1)
 
     return df
 
+@dask.delayed
+def seconds_day(day_gregorate = '2022-08-20', periods = 86400):
+    """Dataframe con los 84,000 segundos del día"""
+    df_time = pd.DataFrame(dict(Time = pd.Series(pd.date_range(f'{day_gregorate} 00:00:00',
+                                                               periods = periods, freq = 's'))))
+    df_time["second_day"] = df_time.index
+    df_time = dd.from_pandas(df_time, npartitions = 1)
+    
+    return df_time
+
+@dask.delayed
+def missing_groups(df,
+                   value= 'zero',
+                   day_gregorate = '2022-08-20'):
+    """Detección de casos de datos faltantes.
+    Porcentaje de 0
+    Porcentaje de nullos
+    Porcentaje de distintos de cero
+    """
+
+    def percentage_zero(df, value = value):
+        
+        if (value == 'zero'):
+            return ((df == 0) & (df.notnull())).sum(axis=0)
+        elif (value == 'no_zero'):
+            return ((df != 0) & (df.notnull())).sum(axis=0)
+        else:
+            return df.isnull().sum(axis=0)
+    
+    if (value == 'zero'):
+        pct_val = "pct_val_zero"
+    elif (value == 'no_zero'):
+        pct_val = "pct_val_no_zero"
+    else:
+        pct_val = "pct_val_null"
+        
+    df["day"] = df['Time'].dt.floor("D")
+    df = df.groupby(["day"]).apply(percentage_zero)
+    df = df/86400
+    df = df.drop(['Time', 'second_day', 'day'], axis =1)
+    df = df.transpose().reset_index()
+    df = pd.melt(df, id_vars=["index"], value_vars=df.columns[1:])
+    
+    df.columns = ["signals","day",pct_val]
+    df = df[df[pct_val] > 0]
+    df = df.sort_values(by = pct_val, ascending = False)
+    
+    return df
