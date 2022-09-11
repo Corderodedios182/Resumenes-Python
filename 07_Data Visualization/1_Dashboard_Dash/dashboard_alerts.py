c9188216e38352d0fc97b21c60c3dd557ab5a9bf
Carlos Roberto Flores Luna <cflorelu@emeal.nttdata.com>
2022-09-11 16:20:46 -0500

    Dash : New Format and update sctructure

--- 07_Data Visualization/1_Dashboard_Dash/dashboard_alerts.py
+++ 07_Data Visualization/1_Dashboard_Dash/dashboard_alerts.py
@@ -4,8 +4,6 @@ Created on Sun Sep 11 09:45:06 2022
 
 @author: cflorelu
 """
-
-# Import required libraries
 import pickle
 import copy
 import pathlib
@@ -19,6 +17,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 import plotly.graph_objects as go
+import plotly.express as px
 
 app = dash.Dash(
     __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}],
@@ -26,15 +25,7 @@ app = dash.Dash(
 app.title = "Señal Alertas"
 server = app.server
 
-#Load data
-df_day = pd.read_csv("data/df_day.csv")
-df_day["signal"] = df_day["llave_comparativa"].str.split("|",expand=True).iloc[:,0]
-
-df_missing_groups = pd.read_csv("data/df_missing_groups.csv")
-
-signals = list(set(df_day["signal"]))
-
-df_outlier = pd.read_csv("data/df_outlier.csv").iloc[:,1:]
+df_final = pd.read_csv("data/df_final.csv")
 
 layout = dict(
     autosize=True,
@@ -53,8 +44,10 @@ app.layout = html.Div([
         #Barra de filtros
         html.Div([
             
-            html.P("Selección de señales : ", className="control_label"),
-            dcc.Dropdown(signals, id ='input_select',multi=True)],
+            html.P("País : ", className="control_label"),
+            dcc.Dropdown(df_final["pais"].unique(),
+                         id ='input_select',
+                         multi=True)],
             
             className="pretty_container four columns",
             id="cross-filter-options"
@@ -64,31 +57,17 @@ app.layout = html.Div([
         html.Div([
             
             html.Div([
-                html.Div([html.H6(id="well_text"), 
-                          html.P("No. de señales")],
-                         id="wells",
-                         className="mini_container"),
-                html.Div([html.H6(id="gasText"),
-                          html.P("Gas")],
-                         id="gas",
-                         className="mini_container"),
-                html.Div([html.H6(id="oilText"),
-                          html.P("Oil")],
-                         id="oil",
-                         className="mini_container"),
-                
-                html.Div([html.H6(id="waterText"),
-                          html.P("Water")],
-                         id="water",
-                         className="mini_container")],
+                html.Div([html.H6(id="n_signal"), html.P("No. de señales")], id="number_signal", className="mini_container"),
+                html.Div([html.H6(id="n_estables"), html.P("Estables")], id="estables", className="mini_container"),
+                html.Div([html.H6(id="n_revision"), html.P("Revisión")], id="revision", className="mini_container")
+                    ],
                 id="info-container",
                 className="row container-display"
                     ),
             
             #Visualizaciones
             html.Div([
-                dcc.Graph(id = 'fig'),
-                dcc.Graph(id='fig_table')],
+                dcc.Graph(id = 'fig')],
                 id="countGraphContainer",
                 className="pretty_container")],
             id="right-column",
@@ -116,59 +95,15 @@ def update_layouts(selection):
         
     fig = go.Figure()
         
-    df_box = df_day[df_day.stack().str.contains('|'.join(title)).any(level=0)]
-
-    fig.add_trace(go.Box(
-        x = df_box["llave_comparativa"],
-        y = df_box["value"],
-        name='kale',
-        boxpoints='all',
-        jitter=0.5,
-        whiskerwidth=0.2,
-        marker_size=2,
-        line_width=1)
-        )
-
-    return fig
-
-@app.callback(
-    Output(component_id = 'fig_table',
-           component_property = 'figure'),
-    Input(component_id = 'input_select',
-           component_property = 'value')
-     )
-def update_table(selection):
-
-    title = 'None'
-    if selection:
-        
-        title = selection
-        
-    fig = go.Figure()
+    tmp = df_final.groupby(["pais","linea","segmento","status_alerta"], as_index = False).count().iloc[:,:5]
+    tmp["x"] = tmp['pais'] + "|" + tmp['linea'].astype(str) + "|" + tmp['segmento'].astype(str)
+    tmp = tmp.iloc[:,[5,3,4]]
+    tmp.columns = ["x","status_alerta","y"]
+    tmp = tmp[tmp.stack().str.contains('|'.join(title)).any(level=0)]
     
-    df_outlier_ = df_outlier[df_outlier.stack().str.contains('|'.join(title)).any(level=0)]
-
-    trace_0 = go.Figure(data=[go.Table(
-        header=dict(values=list(df_outlier_.columns),
-                    fill_color='paleturquoise',
-                    align='left'),
-        cells=dict(values=[df_outlier_.señal, df_outlier_.grado_acero, df_outlier_.velocidad_linea, df_outlier_.ancho_slab,
-                           df_outlier_.dia, df_outlier_.status_outlier, df_outlier_.pct_comparativo_mayo22],
-                   fill_color='lavender',
-                   align='left'))
-    ])
-
-    layout_0 = go.Layout(legend = {"x":.9,"y":.5},  margin=dict(l=20, r=20, t=20, b=20),
-                         height = 4400,
-                         showlegend = False,
-                         paper_bgcolor='rgb(243, 243, 243)',
-                         template = 'ggplot2',
-                         plot_bgcolor='rgb(243, 243, 243)')
-
-    fig = go.Figure(data = trace_0,
-                          layout = layout_0)
+    fig = px.bar(tmp, x="x", y="y", color="status_alerta", title="Detalle General : País | Línea | Segmento")
+    fig.show()
 
     return fig
-
 
 app.run_server(debug=True, use_reloader=False)
