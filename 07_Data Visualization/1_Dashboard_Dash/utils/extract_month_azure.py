747aba83e00a63f015cebdc9d3bdeb6275fbc072
Corderodedios182 <corderodedios182@gmail.com>
2022-10-03 14:52:38 -0500

    Backend : Update extract info azure

--- 07_Data Visualization/1_Dashboard_Dash/utils/extract_month_azure.py
+++ 07_Data Visualization/1_Dashboard_Dash/utils/extract_month_azure.py
@@ -16,18 +16,10 @@ import pandas as pd
 
 def date_range_to_be_extracted(day_gregorate = datetime.today()):
     """
-    Parameters
-    ----------
-    day_gregorate : TYPE, optional
-        DESCRIPTION. The default is datetime.today().
-
     Returns
     -------
-    date_range : TYPE
-        Listado de fechas para lectura de datos ejemplo :
-            
-            [20220927, 20220928, 20220929, 20220930, 20221002]
-
+    date_range : Listado de fechas para lectura de datos ejemplo :
+                 date_range = [20220927, 20220928, 20220929, 20220930, 20221002]
     """
     
     day_gregorate_start = datetime.today() + timedelta(days =-3)
@@ -53,14 +45,13 @@ def date_range_to_be_extracted(day_gregorate = datetime.today()):
     
     return date_range
 
-def azure_data_extraction():
+def status_download_files():
     """
     Returns
     -------
-    TYPE
-        Valida los archivos existentes en : data/ddf_signal
-            
-        Para lectura en caso de que no existan los descarga de Azure.
+    status_extraction : Diccionario que indica si extraer datos, ejemplo:
+                        status_extraction = {'status': True,
+                                             'date_range': [20220927]}
     """
     
     date_range = date_range_to_be_extracted()
@@ -75,16 +66,41 @@ def azure_data_extraction():
     
     date_range = list(date_range[date_range["faltantes"].isin([False])]["date_range"])
     
-    for i in date_range:
+    if len(date_range) == 0:
+        status = False
+    else: status = True
+    
+    status_extraction = {"status": status,
+                         "date_range": date_range}
+    
+    return status_extraction
+
+def azure_data_extraction():
+    """
+    Returns
+    -------
+        Descarga de datos de azure dependiendo del diccionario de 
+        status_download_files()
+    """
+    
+    status_download = status_download_files()
+    
+    if status_download["status"] == False:
         
-        ddf_signal = dd.read_parquet(f'abfs://arg-landing-iba-sns-ccd2@prodllanding.blob.core.windows.net/date={i}/*.parquet',
-                                        storage_options = {"account_name": values.config_values['Signals']['account_name'],
-                                                           "sas_token": values.config_values['Signals']['sas_token']},
-                                        blocksize = None,
-                                        columns = values.config_values['Signals']['columns_file'])
+        status_azure = "No se descargaron archivos de azure"
         
-        ddf_signal = transformations.format_groups(ddf_signal).compute()
+    else:
+        status_azure = []
+        for i in status_download["date_range"]:
+            
+            ddf_signal = dd.read_parquet(f'abfs://arg-landing-iba-sns-ccd2@prodllanding.blob.core.windows.net/date={i}/*.parquet',
+                                         storage_options = {"account_name": values.config_values['Signals']['account_name'],
+                                                            "sas_token": values.config_values['Signals']['sas_token']},
+                                         blocksize = None,
+                                         columns = values.config_values['Signals']['columns_file'])
         
-        ddf_signal.to_csv(r"data/ddf_signal/ddf_signal_{day}.csv".format(day = i), index=False)
+            ddf_signal = transformations.format_groups(ddf_signal).compute()
         
-    return print("Archivo: ", r"ddf_signal_{day}.csv".format(day = i), "guardado")
+            ddf_signal.to_csv(r"data/ddf_signal/ddf_signal_{day}.csv".format(day = i), index=False)
+            status_azure.append(i)
+    return status_azure
