65bf84a50c80856ff82c4114f2d765e367e2b363
Corderodedios182 <corderodedios182@gmail.com>
2022-10-30 23:12:12 -0600

    NLP : Named entity recognition

--- 04_Machine Learning Scientist/12_Natural_Lenguague/2_Intro_NLP_named-entity recognition.py
+++ 04_Machine Learning Scientist/12_Natural_Lenguague/2_Intro_NLP_named-entity recognition.py
@@ -35,7 +35,12 @@ from nltk.tokenize import word_tokenize
 from nltk.tokenize import regexp_tokenize
 from nltk.tokenize import TweetTokenizer #The nltk.tokenize.TweetTokenizer class gives you some extra methods and attributes for parsing tweets.
 
-corpus = str(pd.read_parquet(path = "raw/búsquedas_con_retro_recortado.parquet").loc[:,"TEXTO_COMPARACION"].unique()).replace("]", "").replace("[", "").replace("'", "").replace("\n", "")
+import matplotlib.pyplot as plt
+from collections import defaultdict
+
+with open('data_examples/articles.txt', encoding='utf8') as f:
+    corpus = f.read()
+    print(corpus)
 
 # Tokenize the article into sentences: sentences
 sentences = nltk.sent_tokenize(corpus)
@@ -53,4 +58,70 @@ chunked_sentences = nltk.ne_chunk_sents(pos_sentences, binary=True)
 for sent in chunked_sentences:
     for chunk in sent:
         if hasattr(chunk, "label") and chunk.label() == "NE":
-            print(chunk)
+            print(chunk)     
+
+# Create the defaultdict: ner_categories
+ner_categories = defaultdict(int)
+
+# Create the nested for loop
+for sent in chunked_sentences:
+    for chunk in sent:
+        if hasattr(chunk, 'label'):
+            ner_categories[chunk.label()] += 1
+            
+# Create a list from the dictionary keys for the chart labels: labels
+labels = list(ner_categories.keys())
+
+# Create a list of the values: values
+values = [ner_categories.get(v) for v in labels]
+
+# Create the pie chart
+plt.pie(values, labels=labels, autopct='%1.1f%%', startangle=140)
+
+# Display the chart
+plt.show()
+
+# -- Intro a SpaCy -- #
+
+# Import spacy
+import spacy
+
+# Instantiate the English model: nlp
+nlp = spacy.load('en_core_web_sm', disable=['tagger', 'parser', 'matcher'])
+
+# Create a new document: doc
+doc = nlp(corpus)
+     
+# Print all of the found entities and their labels
+for ent in doc.ents:
+    print(ent.label_, ent.text)
+
+# --Polyglot -- #
+from polyglot.text import Text
+
+# Create a new text object using Polyglot's Text class: txt
+txt = Text(corpus)
+
+# Print each of the entities found
+for ent in txt.entities:
+    print(ent)
+    
+# Print the type of ent
+print(type(ent))
+
+# Initialize the count variable: count
+count = 0
+
+# Iterate over all the entities
+for ent in txt.entities:
+    # Check whether the entity contains 'Márquez' or 'Gabo'
+    if "Márquez" in ent or "Gabo" in ent:
+        # Increment count
+        count += 1
+
+# Print count
+print(count)
+
+# Calculate the percentage of entities that refer to "Gabo": percentage
+percentage = count / len(txt.entities)
+print(percentage)
