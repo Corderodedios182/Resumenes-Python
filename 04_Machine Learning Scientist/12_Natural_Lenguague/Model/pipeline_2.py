49b873a3b8efc672668400f445608a960f99cae7
Corderodedios182 <corderodedios182@gmail.com>
2022-11-02 17:48:41 -0600

    NLP : Report

--- 04_Machine Learning Scientist/12_Natural_Lenguague/Model/pipeline_2.py
+++ 04_Machine Learning Scientist/12_Natural_Lenguague/Model/pipeline_2.py
@@ -39,8 +39,8 @@ datos_model = datos.iloc[:,[5,7,8,9,6]]
 # Características del conjunto de datos
 
 x_entrna, y_entrna, x_valida, y_valida, x_prueba, y_prueba = split_data(datos_a_dividir = datos,
-                                                                        p_explr_prueba = .90,
-                                                                        p_entrn_valida = .89,
+                                                                        p_explr_prueba = .80,
+                                                                        p_entrn_valida = .80,
                                                                         )
 
 noms_cols_caracts = ['TEXTO_COMPARACION_VECT',
@@ -71,46 +71,6 @@ feats_prueba, labels_prueba, qids_prueba = \
     )
 
 #-- ModeloXGbosstRanker --#
-#Simple model
-
-from sklearn.metrics import average_precision_score, ndcg_score
-from sklearn.model_selection import GroupShuffleSplit
-import xgboost
-
-params = {'objective': 'rank:ndcg',
-          'eval_metric': ['map','ndcg','ndcg@5','ndcg@10','rmse','auc']}
-
-model_l2r = xgboost.XGBRanker(**params)
-
-model_l2r.fit(X = feats_entrna,
-              y = labels_entrna,
-              qid = qids_entrna,
-              eval_set = [(feats_entrna, labels_entrna), 
-                          (feats_valida, labels_valida)],
-              eval_qid = [qids_entrna,
-                          qids_valida,],
-              verbose = False)
-
-evals_result = model_l2r.evals_result()
-
-print('Los 5 últimos:')
-print()
-print(f"MAPs (entrenamiento): {evals_result['validation_0']['map'][-5:]}")
-print(f"NDCGs (entrenamiento): {evals_result['validation_0']['ndcg'][-5:]}")
-print(f"NDCG@5s (entrenamiento): {evals_result['validation_0']['ndcg@5'][-5:]}")
-print(f"NDCG@10s (entrenamiento): {evals_result['validation_0']['ndcg@10'][-5:]}")
-print(f"RMSEs (entrenamiento): {evals_result['validation_0']['rmse'][-5:]}")
-print(f"AUCs (entrenamiento): {evals_result['validation_0']['auc'][-5:]}")
-print()
-print(f"MAPs (validación): {evals_result['validation_1']['map'][-5:]}")
-print(f"NDCGs (validación): {evals_result['validation_1']['ndcg'][-5:]}")
-print(f"NDCG@5s (validación): {evals_result['validation_1']['ndcg@5'][-5:]}")
-print(f"NDCG@10s (validación): {evals_result['validation_1']['ndcg@10'][-5:]}")
-print(f"RMSEs (validación): {evals_result['validation_1']['rmse'][-5:]}")
-print(f"AUCs (validación): {evals_result['validation_1']['auc'][-5:]}")    
-
-y_pred_train = model_l2r.predict(feats_entrna)
-
 # -- Hypertuning -- #
 
 from sklearn.model_selection import cross_val_score, KFold
@@ -138,8 +98,8 @@ def grid_search(params, xgboost = xgboost.XGBRanker()):
     print("Best params:", best_params)
 
 grid_search(params = {'objective': ['rank:ndcg'],
-                      'reg_lambda' : [.01,.1,.5,1],
-                      'subsample':[0.4, 0.5, 0.6, 0.7]})
+                      'reg_lambda' : [.10, .15, .21, .25, 30],
+                      'subsample':[0.3, 0.4, 0.5, 0.6, 0.7]})
 
 # -- BestModel metrics -- #
 
@@ -157,7 +117,7 @@ model_l2r.fit(X = feats_entrna,
               eval_set = [(feats_entrna, labels_entrna), 
                           (feats_valida, labels_valida)],
               eval_qid = [qids_entrna,
-                          qids_valida,],
+                          qids_valida],
               verbose = False)
 
 evals_result = model_l2r.evals_result()
@@ -177,3 +137,23 @@ print(f"NDCG@5s (validación): {evals_result['validation_1']['ndcg@5'][-5:]}")
 print(f"NDCG@10s (validación): {evals_result['validation_1']['ndcg@10'][-5:]}")
 print(f"RMSEs (validación): {evals_result['validation_1']['rmse'][-5:]}")
 print(f"AUCs (validación): {evals_result['validation_1']['auc'][-5:]}")    
+
+# -- Reportes -- #
+#Datos de entrenamiento
+
+x_prueba = pd.DataFrame(x_prueba)
+
+y_prueba = pd.DataFrame(y_prueba).reset_index()
+
+y_pred_prueba = model_l2r.predict(feats_prueba)
+
+y_prueba["y_pred_prueba"] = y_pred_prueba
+
+results = pd.merge(datos,
+                   y_prueba,
+                   on = 'index',
+                   how = 'inner')
+
+#Generar rank por modelo.
+
+
